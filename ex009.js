7864e6a4568ce67dfe08a0a90e3dbd4128e86a7c
Carlos Eduardo <152689222+apolinariocarlos@users.noreply.github.com>
2023-12-04 07:02:22 -0300

    Condições Compostas.

---
+++ ex009.js
@@ -0,0 +1,7 @@
+var país = `Brasil`
+console.log(`Vivendo em ${país}`)
+if (país == `Brasil`) { // Condições Compostas.
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
